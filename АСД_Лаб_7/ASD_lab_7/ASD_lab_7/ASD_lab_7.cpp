a2c604d11347d637ffcfaf77f73fc3a2b3320a77
cmdax <90183750+cmdax@users.noreply.github.com>
2021-12-14 19:00:45 +0200

    Add files via upload

---
+++ АСД_Лаб_7/ASD_lab_7/ASD_lab_7/ASD_lab_7.cpp
@@ -0,0 +1,66 @@
+﻿#include <ctime>
+#include <iostream>
+
+using namespace std;
+
+const int SIZE = 10;
+
+void fillArrays(char* , char*);
+void thirdArrFill(char*, char*, char*);
+int countRes(char*);
+void charArrOutput(char*, string);
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    char arr1[SIZE], arr2[SIZE], arr3[SIZE] = {0};
+
+    fillArrays(arr1, arr2);
+    thirdArrFill(arr1, arr2, arr3);
+
+    charArrOutput(arr1, "First arr elements:");
+    charArrOutput(arr2, "Second arr elements:");
+    charArrOutput(arr3, "Third arr elements:");
+    
+    cout << "Result: " << countRes(arr3)<<endl;
+
+}
+
+
+void charArrOutput(char* arr, string msg) {
+    cout << msg << endl;
+    for (int i = 0; i < SIZE; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl << endl;
+}
+
+void fillArrays(char* arr1, char* arr2) {
+    for (int i = 0; i < SIZE; i++) {
+            arr1[i] = 100 + i;
+            arr2[i] = 110 - i * i;
+    }   
+}
+
+void thirdArrFill(char* arr1, char* arr2, char* arr3) {
+    int k = 0;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (arr1[i] == arr2[j]) {
+                arr3[k] = arr1[i];
+                k++;
+            }
+        }
+    }
+}
+
+
+int countRes(char* arr3) {
+    int res = 1;
+    for (int i = 0; i<SIZE; i++) {
+        if(arr3[i]!=0)
+            res *= int(arr3[i]);
+    }
+    return res;
+}
